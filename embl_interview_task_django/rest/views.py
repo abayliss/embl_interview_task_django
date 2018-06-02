f483b2728f9752cbc40799aaad372fb9eda8d347
Andrew Bayliss <abayliss@gmail.com>
2018-06-02 15:09:23 +0100

    Basic authentication required to use the API

--- embl_interview_task_django/rest/views.py
+++ embl_interview_task_django/rest/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from embl_interview_task_django.rest.models import Person
 from embl_interview_task_django.rest.serialzers import PersonSerializer
 
 
 class PersonViewSet(viewsets.ModelViewSet):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Person.objects.all().order_by('last_name')
     serializer_class = PersonSerializer
